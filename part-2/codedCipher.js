6e16adb3d7fa04d765001e4f90fa6bc67ded23d1
Marcus Connors <biffrocker1@gmail.com>
2022-03-03 16:03:30 -0700

    updated cipher

--- part-2/codedCipher.js
+++ part-2/codedCipher.js
@@ -174,3 +174,20 @@ function decodedStr(input) {
 
 console.log(codedStr("hello"))
 console.log(decodedStr('svool'))
+ // this is the solution me and yama and amie figured out together
+let array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '!', ' ', '$', '#', '%', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+function encryptString(str){
+    let encryptedString = '';
+    for (let index = 0; index < str.length; index++) {
+        let key = (array.length-1) - array.indexOf(str[index])
+
+        encryptedString += array[key];
+    }
+    return encryptedString;
+}
+
+let test = 'I love cryptography!';
+let test2 = encryptString(test);
+
+console.log(encryptString(test));
